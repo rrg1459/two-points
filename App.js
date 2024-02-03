21b799a0748da1d9257cfa1f8318de3faa9df57c
rafaDev <rrg1459@hotmail.com>
2024-02-03 15:30:37 -0400

    Add Marker

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import MapView from 'react-native-maps';
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import MapView, { Marker } from 'react-native-maps';
+
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [position, setPosition] = useState({
+    latitude: 10.600757623910244,
+    longitude: -67.02293348859472,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setPosition({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}></MapView>
+      <MapView
+        style={styles.map}
+        initialRegion={position}
+        region={position}></MapView>
       <StatusBar style="auto" />
+      <Marker
+        coordinate={{
+          latitude: position.latitude,
+          longitude: position.longitude
+        }}
+        tracksViewChanges={true}>
+      </Marker>
     </View>
   );
 }
