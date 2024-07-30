c19c2b4e67beffc056f0b27a54f2b0a2e8d24260
rafaDev <rafa.rodriguez@skydropx.com>
2024-07-30 14:00:14 -0400

    add metro config

---
+++ metro.config.js
@@ -0,0 +1,5 @@
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const config = {};
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
